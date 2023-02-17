761ef2868c697aafada7371fff54fe15021f99ee
Aman-byte <57094534+Aman-byte@users.noreply.github.com>
2023-02-17 23:27:33 +0530

    Create app.js

---
+++ app.js
@@ -0,0 +1,7 @@
+function showDescription() {
+  document.getElementById('descriptionPopup').style.display = 'block';
+}
+
+function hideDescription() {
+  document.getElementById('descriptionPopup').style.display = 'none';
+}
